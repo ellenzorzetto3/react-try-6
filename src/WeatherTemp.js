57b17a645715de24e7f89313df574fdadb82ab61
Ellen Zorzetto <ellen.zorzetto.it@gmail.com>
2024-07-29 00:29:28 -0300

    finished the metric-imperial systrem

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,16 +1,43 @@
 import React, { useState } from "react";
+import "./WeatherTemp.css";
 
 export default function WeatherTemp(props) {
-    const [unit, setUnit] = useState("celsius")
+  const [unit, setUnit] = useState("celsius");
 
-    function convertToFahrenheit(){}
-    if(unit === "celsius") {
-  return (
-    <div className="WeatherTemp">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">
-        °C | <a href="/" onClick={convertToFahrenheit}>ºF</a>
-      </span>
-    </div>
-  );
-}else {return ("ºF")}}
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="WeatherTemp">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            ºF
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+    return (
+      <div className="WeatherTemp">
+        <span className="temperature">{Math.round(fahrenheit)}</span>
+        <span className="unit">
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>{" "}
+          | ºF
+        </span>
+      </div>
+    );
+  }
+}
