1e97fe52111e10d3a0a44709fce5fd8efa92284c
Ellen Zorzetto <ellen.zorzetto.it@gmail.com>
2024-08-04 22:34:36 -0300

    more updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Weather from "./Weather";
@@ -10,16 +9,23 @@ function App() {
         <Weather defaultCity="Paris" />
         <footer>
           This project was coded by{" "}
-          <a href="https://www.linkedin.com/in/ellen-zorzetto-1a1777b5/">
+          <a
+            href="https://www.linkedin.com/in/ellen-zorzetto-1a1777b5/"
+            target="_blank"
+          >
             Ellen Zorzetto
           </a>{" "}
           and is open-sourced on{" "}
-          <a href="https://github.com/ellenzorzetto3/react-try-6">GitHub</a>
+          <a
+            href="https://github.com/ellenzorzetto3/react-try-6"
+            target="_blank"
+          >
+            GitHub
+          </a>
         </footer>
       </div>
     </div>
   );
-  
 }
 
 export default App;
